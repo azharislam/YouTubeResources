57049d5426e7ddc9d9399a5c09a799dd7553fa99
Azhar Islam <azhartislam@gmail.com>
2022-04-22 20:50:55 +0100

    Filter out nils and combine two publishers

--- 05 - Getting Started with Combine - Base Project/Combining Subscriptions/Combining Subscriptions/CombiningSubscriptions.playground/Contents.swift
+++ 05 - Getting Started with Combine - Base Project/Combining Subscriptions/Combining Subscriptions/CombiningSubscriptions.playground/Contents.swift
@@ -8,3 +8,15 @@ import Combine
 
 let meals: Publishers.Sequence<[String?], Never> = ["🍔", "🌭", "🍕", nil].publisher
 let people: Publishers.Sequence<[String?], Never> = ["Tunde", "Bob", "Toyo", "Jack"].publisher
+
+// Never = never fails
+
+let subscription = people
+    .zip(meals) // Combine meals and people
+    .filter({ $0 != nil && $1 != nil}) // Filter out the nils
+    .sink { completion in
+        print("Subscription: \(completion)") // Print out completion of subscription
+    } receiveValue: { (person, meal) in // Have access to person and meal from RecVal
+        print("\(person) enjoys \(meal)") // Listen to what they emit, create custom string
+    }
+}
