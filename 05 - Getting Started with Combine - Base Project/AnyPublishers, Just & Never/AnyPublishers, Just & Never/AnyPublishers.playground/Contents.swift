a61bcc2a7573e1f9f41f0bd12bca0d054d211162
Azhar Islam <azhartislam@gmail.com>
2022-04-23 19:54:53 +0100

    Test network call using Combine

--- 05 - Getting Started with Combine - Base Project/AnyPublishers, Just & Never/AnyPublishers, Just & Never/AnyPublishers.playground/Contents.swift
+++ 05 - Getting Started with Combine - Base Project/AnyPublishers, Just & Never/AnyPublishers, Just & Never/AnyPublishers.playground/Contents.swift
@@ -9,3 +9,24 @@ struct Post: Codable {
 }
 
 let postURL = URL(string: "https://jsonplaceholder.typicode.com/posts")
+
+func getPosts() -> AnyPublisher<[Post], Never> {
+    
+    let postPublisher = URLSession // Create URL session for internet work
+        .shared
+        .dataTaskPublisher(for: postURL!) // Call service from URL
+        .map { $0.data } // Map data from service
+        .decode(type: [Post].self, decoder: JSONDecoder()) // Decode data to Array of posts
+        .catch { _ in return Just([]) } // If fails, JUST return empty posts
+        .eraseToAnyPublisher() // Turns it into an AnyPublisher
+    
+    return postPublisher
+}
+
+var subscriptions = Set<AnyCancellable>()
+
+getPosts()
+    .sink { posts in
+        print(posts)
+    }
+    .store(in: &subscriptions)
